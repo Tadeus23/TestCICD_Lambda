c01437766085cfc2579f1cee27e5a8ab2f3cba6a
Tadeus23 <popescu351@gmail.com>
2024-05-21 16:46:25 +0300

    feat: Add Github action :construction_worker:

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,5 +3,8 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!- Updated V11')
+        'body': json.dumps('Hello from Lambda!')
     }
+
+if __name__ == '__main__':
+    print(lambda_handler({}, {}))
